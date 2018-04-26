b8f27b78d3dadcee69a7e88e3c759551f3ecf0c6
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2018-04-26 15:59:13 +0200

    AXLE-1 Add basic acceptance test framework

---
+++ src/Tests/Axle.Tests.Acceptance/ConcurrentSessionsFeature.cs
@@ -0,0 +1,19 @@
+﻿namespace Axle.Tests.Acceptance
+{
+    using System.Threading;
+    using Xbehave;
+    using Xunit;
+
+    [Collection("Axle collection")]
+    public class ConcurrentSessionsFeature
+    {
+        [Scenario]
+        public void OpeningANewSessionShouldCloseAnOlderActiveSession()
+        {
+            "Given nothing happens".x(() =>
+            {
+                Assert.True(1 == 1);
+            });
+        }
+    }
+}
---
+++ src/Tests/Axle.Tests.Acceptance/Support/AxleAcceptanceTestCollection.cs
@@ -0,0 +1,12 @@
+﻿namespace Axle.Tests.Acceptance.Support
+{
+    using Xunit;
+
+    [CollectionDefinition("Axle collection")]
+    public class AxleAcceptanceTestCollection : ICollectionFixture<AxleFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
