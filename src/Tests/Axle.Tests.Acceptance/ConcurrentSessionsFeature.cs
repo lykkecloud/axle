315985ab7696992dc91cb2574a68577781e7e3dc
Marta Ciesielska <marta.h.ciesielska@gmail.com>
2018-04-30 22:41:58 +0200

    AXLE-7 Add horrendous acceptance test to check if concurrent requests are always handled correctly

---
+++ src/Tests/Axle.Tests.Acceptance/ConcurrentSessionsFeature.cs
@@ -0,0 +1,99 @@
+﻿namespace Axle.Tests.Acceptance
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Axle.Tests.Acceptance.Support;
+    using FluentAssertions;
+    using Microsoft.AspNetCore.SignalR.Client;
+    using Xbehave;
+    using Xunit;
+
+    [Collection("Axle2")]
+    public class ConcurrentSessionsFeature : IntegrationTest
+    {
+        public ConcurrentSessionsFeature(AxleFixture axleFixture)
+            : base(axleFixture)
+        {
+        }
+
+        [Scenario]
+        public void OpeningMultipleSessionsShouldKeepOnlyOneActiveSession()
+        {
+            var numberOfSessions = 3;
+            var signalRClients = Enumerable.Range(0, numberOfSessions).Select(x => new SignalRClient(this.AxleUrl)).ToArray();
+            var threads = Enumerable.Range(0, numberOfSessions).Select(x => new Thread(this.StartSessionSynchronously)).ToArray();
+
+            "Given multiple connections to Axle"
+                .x(async () =>
+                {
+                    foreach (var client in signalRClients)
+                    {
+                        await client.StartConnection();
+                    }
+                });
+
+            "When I try to open multiple sessions with the same user ID at once"
+                .x(() =>
+                {
+                    for (int i = 0; i < numberOfSessions; i++)
+                    {
+                        threads[i].Start(signalRClients[i]);
+                    }
+                });
+
+            "Then only one session remains connected"
+                .x(async () =>
+                {
+                    await Task.Delay(100);
+                    var connected = 0;
+                    var disconnected = 0;
+
+                    foreach (var client in signalRClients)
+                    {
+                        if (client.IsConnected)
+                        {
+                            connected++;
+                        }
+                        else
+                        {
+                            disconnected++;
+                        }
+                    }
+
+                    connected.Should().Be(1);
+                    disconnected.Should().Be(numberOfSessions - 1);
+                })
+                .Teardown(async () =>
+                {
+                    foreach (var thread in threads)
+                    {
+                        thread.Join();
+                    }
+
+                    foreach (var client in signalRClients)
+                    {
+                        await client.Teardown();
+                    }
+                });
+        }
+
+        private void StartSessionSynchronously(object obj)
+        {
+            // TODO (Marta): This is quite horrendous, I should implement a unit test for the concurrency stuff instead.
+            var client = (SignalRClient)obj;
+            try
+            {
+                client.StartSession("abc").Wait();
+            }
+            catch (AggregateException aex)
+            {
+                if (aex.InnerException.GetType() != typeof(HubException))
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ src/Tests/Axle.Tests.Acceptance/Support/Axle2Collection.cs
@@ -0,0 +1,12 @@
+﻿namespace Axle.Tests.Acceptance.Support
+{
+    using Xunit;
+
+    [CollectionDefinition("Axle2")]
+    public class Axle2Collection : ICollectionFixture<AxleFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
