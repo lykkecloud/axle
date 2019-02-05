bd16eef36b31869a44a780135be0ae78f94ed79c
waqas zamir <waqas.zamir@lykke.com>
2019-02-05 15:35:17 +0500

    AXLE-34 exposed endpoint to get session id by user id

---
+++ src/Axle/Controllers/SessionsController.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace Axle.Controllers
+{
+    using System.Net;
+    using Axle.Dto;
+    using Axle.Persistence;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using NSwag.Annotations;
+
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionsController : Controller
+    {
+        private readonly ISessionRepository sessionRepository;
+
+        public SessionsController(ISessionRepository sessionRepository)
+        {
+            this.sessionRepository = sessionRepository;
+        }
+
+        [HttpGet("{userId}")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(UserSessionResponse))]
+        public IActionResult Get(string userId)
+        {
+            var sessionState = this.sessionRepository.GetByUser(userId);
+
+            if (sessionState == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(new UserSessionResponse { UserSessionId = sessionState.SessionId });
+        }
+    }
+}
