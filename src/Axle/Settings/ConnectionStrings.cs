acf93c06681a81b77e8e434b05553e645d069e35
waqas zamir <waqas.zamir@lykke.com>
2019-03-19 09:48:39 +0500

    added missing app settings check

---
+++ src/Axle/Settings/ConnectionStrings.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace Axle.Settings
+{
+    using Lykke.SettingsReader.Attributes;
+
+    public class ConnectionStrings
+    {
+        [AmqpCheck]
+        public string RabbitMq { get; set; }
+    }
+}
