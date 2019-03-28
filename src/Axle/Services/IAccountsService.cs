352f66e28f13a82dc73b73d3a4419fb10c8ecf79
Dafinka Gerdjikova <dafinka.gerdjikova@lykke.com>
2019-03-28 11:59:29 +0200

    LT-1240 update licenses

--- src/Axle/Services/IAccountsService.cs
+++ src/Axle/Services/IAccountsService.cs
@@ -1,4 +1,6 @@
-﻿namespace Axle.Services
+﻿// (c) Lykke Corporation 2019 - All rights reserved. No copying, adaptation, decompiling, distribution or any other form of use permitted.
+
+namespace Axle.Services
 {
     using System.Threading.Tasks;
 
