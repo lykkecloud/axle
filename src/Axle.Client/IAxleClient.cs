927628dbf9054a8dd51de619f6c1d34d991eba70
Alexey Andreev <alexey.andreev@lykke.com>
2019-10-11 17:48:14 +0300

    LT-1707: add to client

--- src/Axle.Client/IAxleClient.cs
+++ src/Axle.Client/IAxleClient.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) 2019 Lykke Corp.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 namespace Axle.Client
 {
     using Axle.Client.Models;
@@ -13,6 +15,13 @@ namespace Axle.Client
     public interface IAxleClient
     {
         [Get("/api/sessions/{userName}")]
+        [Obsolete("Use GET /for-support/{userName} and GET /for-user/{accountId}")]
         Task<UserSessionResponse> GetUserSession([AliasAs("userName")]string userName);
+
+        [Get("/api/sessions/for-support/{userName}")]
+        Task<UserSessionResponse> GetSessionForSupport(string userName);
+
+        [Get("/api/sessions/for-user/{accountId}")]
+        Task<UserSessionResponse> GetSessionForUser(string accountId);
     }
 }
